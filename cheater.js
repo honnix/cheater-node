69524a10229cc14f66ed66d4e01cf2ba865dcfc8
ehonlia <hongxin.liang@ericsson.com>
2011-04-14 18:54:33 +0800

    status

--- cheater.js
+++ cheater.js
@@ -1,7 +1,13 @@
+var cheaterConstant = require('./cheater_constant.js');
+
 var Cheater = function () {
-    this.currentStatus = 1;
+    this.currentStatus = cheaterConstant.statusMap.initializing;
 };
 
 Cheater.prototype.canStart = function () {
-    return this.currentStatus === 1;
+    return this.currentStatus.val === 1;
 };
+
+exports.create = function () {
+    return new Cheater();
+}
